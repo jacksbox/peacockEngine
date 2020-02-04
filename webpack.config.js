1ed62cf7677e8331e85c97455776a6b21298f39c
jacomo <github@jacksbox.de>
2020-02-04 15:36:17 +0100

    add babel and setup webpack babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,21 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-  },
-};
+  }
+}
