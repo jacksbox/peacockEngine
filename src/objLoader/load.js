7c0c0c1cf03cdec3fefdd678e5c02a4470223d3b
jacomo <github@jacksbox.de>
2020-02-05 18:47:46 +0100

    parse mtl materials (colors only)

--- src/objLoader/load.js
+++ src/objLoader/load.js
@@ -1,9 +1,20 @@
 import loadFile from './loadFile'
 import parseObj from './parseObj'
+import parseMtl from './parseMtl'
 
-const load = async url => {
-  const text = await loadFile(url)
-  const objData = parseObj(text)
+const REGEX_MTL = /^mtllib\s+(.*)$/m
+
+const load = async (path, url) => {
+  const objFile = await loadFile(`${path}/${url}`)
+
+  let mtlData = null
+  const [, match = null] = REGEX_MTL.exec(objFile) || [null, null]
+  if (match) {
+    const mtlFile = await loadFile(`${path}/${match}`)
+    mtlData = parseMtl(mtlFile)
+  }
+
+  const objData = parseObj(objFile, mtlData)
 
   return objData
 }
---
+++ src/objLoader/parseMtl.js
@@ -0,0 +1,30 @@
+const REGEX_NEWMTL = /^newmtl\s+(.*)/
+const REGEX_COLORS = /^(Ka|Kd|Ks)\s+([0-9]*\.?[0-9]+)\s+([0-9]*\.?[0-9]+)\s+([0-9]*\.?[0-9]+)/
+
+const parseMtl = text => {
+  const lines = text.split('\n')
+
+  const materials = {}
+  let currentMaterial = null
+  let name
+  let type
+  let r
+  let g
+  let b
+  lines.forEach(line => {
+    ;[, name] = REGEX_NEWMTL.exec(line) || [null, null]
+    if (name) {
+      currentMaterial = name
+      materials[currentMaterial] = {}
+      return
+    }
+    ;[, type, r, g, b] = REGEX_COLORS.exec(line) || [null, null, null, null]
+    if (type) {
+      materials[currentMaterial][type] = [r, g, b]
+    }
+  })
+
+  return materials
+}
+
+export default parseMtl
